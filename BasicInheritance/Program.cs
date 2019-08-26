3f6bda11ca194f5fd0a9464828249dae2fdd7ad3
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 17:35:10 -0400

    Add Cars and Minivan directory and class.

--- BasicInheritance/Program.cs
+++ BasicInheritance/Program.cs
@@ -17,6 +17,11 @@ namespace BasicInheritance
             // Set the current speed, and print it.
             myCar.Speed = 50;
             Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+
+            // Now make a MiniVan object.
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 10;
+            Console.WriteLine("My van is going {0} MPH", myVan.Speed);
             Console.ReadLine();
         }
     }
