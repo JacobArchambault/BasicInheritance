3ba679d85cacb4befc8d9f1e6240621822a1d3a9
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 17:17:08 -0400

    Create and instantiate Car class.

--- BasicInheritance/Program.cs
+++ BasicInheritance/Program.cs
@@ -10,6 +10,14 @@ namespace BasicInheritance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Basic Inheritance *****\n");
+            // Make a Car object and set max speed.
+            Car myCar = new Car(80);
+
+            // Set the current speed, and print it.
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going {0} MPH", myCar.Speed);
+            Console.ReadLine();
         }
     }
 }
